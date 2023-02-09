234364efc4eabaec823bf0c42cf75770800e36f1
renaud-rwemalika <renaud_rwemalika@goodyear.com>
2023-02-09 09:15:51 +0100

    download the data so it works on airflow

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,8 +1,12 @@
+from io import StringIO
 import pandas
 
 from zenml.steps import step, Output
 from pandas import DataFrame
+import urllib
 
 @step
 def load_data() -> Output(data = DataFrame):
-    return pandas.read_csv('data/cardio_train.csv', sep=";")
+    response = urllib.request.urlopen("https://raw.githubusercontent.com/kabinja/zenml-playground/main/data/cardio_train.csv")
+    csv_content = StringIO(str(response.read(),'utf-8'))
+    return pandas.read_csv(csv_content, sep=";")
